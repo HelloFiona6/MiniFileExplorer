8629503df5ae7dfc51056acba28521fa8ed9250e
HelloFiona6 <1870341385@qq.com>
2025-12-31 22:29:40 +0800

    fix: delete out file

--- include/FileSystem.h
+++ include/FileSystem.h
@@ -1,12 +1,21 @@
 #ifndef FILE_SYSTEM_H
 #define FILE_SYSTEM_H
+using namespace std;
 
 #include <string>
 
+
+struct FileInfo {
+    string name;
+    bool isDir;
+    long size;
+    string mtime;
+};
+
 class FileSystem {
 public:
-    static bool exists(const std::string& path);
-    static bool isDir(const std::string& path);
+    static bool exists(const string& path);
+    static bool isDir(const string& path);
 };
 
 #endif
